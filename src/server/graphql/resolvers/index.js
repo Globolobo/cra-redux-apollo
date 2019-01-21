0da7b7ee3a20a1c85284917e52693670116575cb
spich4 <sean.pichardo@nike.com>
2019-01-21 12:55:29 -0800

    pagination server side, cleanup

--- src/server/graphql/resolvers/index.js
+++ src/server/graphql/resolvers/index.js
@@ -1,10 +1,12 @@
 const rp = require('request-promise-native')
 const apiKey = '057dfa32a18eed0f2dc23dc2e80ed8a0'
 const baseUrl = `https://api.themoviedb.org/3/search/movie?page=1&include_adult=false&language=en-US&api_key=${apiKey}`
+const imgUrl = 'https://image.tmdb.org/t/p/original/'
+
 module.exports = {
   Query: {
-    MovieResults: async (_, {title}) => {
-      const searchUrl = `${baseUrl}&query=${title}`
+    MovieResults: async (_, { title, page = 1 }) => {
+      const searchUrl = `${baseUrl}&query=${title}&page=${page}`
       const { results } = await rp({uri: searchUrl, json: true})
       
       return results
@@ -12,10 +14,6 @@ module.exports = {
   },
   Movie: {
     date: ({ release_date }) => release_date,
-    imgSrc: async ({poster_path}) => {
-      console.log('poster_path: ', poster_path);
-      const baseImgUrl = 'https://image.tmdb.org/t/p/original/'
-      return `${baseImgUrl}${poster_path}`
-    }
+    imgSrc: async ({ poster_path }) => `${imgUrl}${poster_path}`
   }
 }
